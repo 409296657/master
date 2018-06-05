2e6887a4932e4f6996eaf0adbccf3b4e1f1d1851
ob <409296657@qq.com>
2018-06-05 23:34:04 +0800

    update

--- src/page/topic/topic.vue
+++ src/page/topic/topic.vue
@@ -3,7 +3,13 @@
     <el-container>
       <el-header height="80px"><Head isActive="4"></Head></el-header>
       <el-main>
-
+        <div class="container">
+          <div class="nodes">
+            <div class="button" v-for="node in nodeList" :class="{active:node.id==nodeid}" @click="nodeid=node.id">
+              <router-link :to="{ path: 'topic/node'+node.id}">{{node.name}}</router-link>
+            </div>
+          </div>
+        </div>
       </el-main>
      </el-container>
    <el-footer></el-footer>
@@ -24,10 +30,25 @@ export default {
   },
   data(){
     return{
+      nodeid:1,
+      nodeList:[],
     }
   },
   methods:{
 
+  },
+  mounted(){
+    this.axios({
+      method:"GET",
+      url:'https://diycode.cc/api/v3/news/nodes.json',
+    })
+    .then((res)=>{
+      console.log(res)
+      this.nodeList = res.data;
+    })
+    .catch((err)=>{
+
+    })
   }
 }
 </script>
@@ -58,5 +79,27 @@ export default {
 }
 .topic #tab-first{
   margin-right: 30px;
+}
+.topic .container .nodes{
+  padding: 20px 15px;
+  background-color: #f9f9f9;
+}
+.topic .container .nodes .button{
+  display: inline-block;
+  padding: 0 10px;
+  height: 20px;
+  font-size: 14px;
+  line-height: 20px;
+  background-color: #aab5c3;
+  border-radius: 10px;
+  margin-right: 15px;
+  margin-bottom: 15px;
+}
+.topic .container .nodes .button.active{
+  background-color: #f87b00;
+}
+.topic .container .nodes .button a{
+  color: #fff;
+  text-decoration: none;
 }
 </style>
